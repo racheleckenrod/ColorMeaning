8a1ea6b61324e39810ca2b9e7519b7883995e8f2
racheleckenrod <rachel@racheleckenrod.com>
2022-08-07 15:55:17 -0500

    updated PORT

--- server.js
+++ server.js
@@ -114,6 +114,6 @@ app.get('/api/:name', (request, response)=>{
    
 })
 
-app.listen(PORT, ()=>{
+app.listen(process.env.PORT || PORT, ()=>{
     console.log(`The server is now running on Port ${PORT}. You better go catch it!`)
 })
