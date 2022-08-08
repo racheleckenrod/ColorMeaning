074b30b202d3b54c13e7fcd05b0f7896ebe46e37
racheleckenrod <rachel@racheleckenrod.com>
2022-08-07 19:10:49 -0500

    additional changes to fix background color changing and more

--- js/main.js
+++ js/main.js
@@ -1,16 +1,17 @@
 document.querySelector('#clickMe').addEventListener('click', makeReq)
 
+const entry = document.querySelector('#entry').value;
 async function makeReq(){
-    const entry = document.querySelector('#entry').value;
+    // const entry = document.querySelector('#entry').value;
     const res = await fetch(`https://color-meanings.herokuapp.com/api/${entry}`)
     const data = await res.json()
 
     console.log(data)
-    document.querySelector('#summary').textContent = data.summary
-    document.querySelector('#symbolizes').textContent = data.symbolizes
-    document.querySelector('#effects').textContent = data.effects
-    document.querySelector('#positive').textContent = data.positive
-    document.querySelector('#negative').textContent = data.negative
+    document.querySelector('#summary').textContent = 'Summary' + " " + data.summary
+    document.querySelector('#symbolizes').textContent = `Symbolizes \n ${data.symbolizes}`
+    document.querySelector('#effects').textContent = `Effects \n ${data.effects}`
+    document.querySelector('#positive').textContent = `Positive \n ${data.positive}`
+    document.querySelector('#negative').textContent = `Negative \n ${data.negative}`
 
 }
 
