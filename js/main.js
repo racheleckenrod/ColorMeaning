11ca1243be0ba65adc979d417e1431f280aabf67
racheleckenrod <rachel@racheleckenrod.com>
2022-08-07 19:01:48 -0500

    tiny fix in fetch

--- js/main.js
+++ js/main.js
@@ -2,7 +2,7 @@ document.querySelector('#clickMe').addEventListener('click', makeReq)
 
 async function makeReq(){
     const entry = document.querySelector('#entry').value;
-    const res = await fetch(`https://color-meanings.herokuapp.com/api?${entry}`)
+    const res = await fetch(`https://color-meanings.herokuapp.com/api/${entry}`)
     const data = await res.json()
 
     console.log(data)
